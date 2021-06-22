aa1349e3fb9211aaebc432c3a4da082f0ad821d2
set.ortiz-monasterio <set.ortiz-monasterio@mongodb.com>
2021-06-21 21:30:58 -0300

    clean code

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,7 @@
+import * as Random from '../utils/random'
 const express = require('express')
 const router = express.Router()
 const path = require('path')
-import * as Random from '../utils/random'
 
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public'))
